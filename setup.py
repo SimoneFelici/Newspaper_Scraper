77c848d9b6e71f12bf520ca99cd0fe70f3055069
Blueaulo <qualcosa588@gmail.com>
2023-06-02 13:24:29 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-from cx_Freeze import setup, Executable
-  
-setup(name = "newscraper" ,
-      version = "2.0.0" ,
-      description = "NEW GUI!!!" ,
-      executables = [Executable("newscraper.py")])
