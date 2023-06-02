21155eb14e3725cb26ac97a994b5c0c812f9c12f
Blueaulo <qualcosa588@gmail.com>
2023-06-02 13:09:18 +0200

    Revert "Changed UI, removed all the bugs."
    This reverts commit 4273d36664638d344fafca69669d7d6f930377d9.

--- sitescraper.py
+++ sitescraper.py
@@ -1,9 +1,9 @@
 import re
+import easygui
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from transformers import BartTokenizer, BartForConditionalGeneration
-from consolemenu import SelectionMenu
 
 def generate_summary(text):
     tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
@@ -15,12 +15,14 @@ def generate_summary(text):
     summary = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
     return summary
 
+
 def scrape_and_display(url):
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "lxml")
         paragraphs = soup.find_all('p')
 
+        print('-------------------')
         text = ''
         for paragraph in paragraphs:
             paragraph_text = paragraph.get_text(strip=True)
@@ -28,8 +30,6 @@ def scrape_and_display(url):
                 formatted_text = re.sub(r'\.(\s|$)', '.\n', paragraph_text)
                 text += formatted_text + '\n'
 
-        # Display the scraped text
-        print('-------------------')
         print(text)
 
         # Prompt the user to generate a summary
@@ -39,17 +39,8 @@ def scrape_and_display(url):
             print("Summary:")
             print(summary)
 
-            # Prompt the user to choose an action
-            action = input("Press 'r' to return to the article selection or 'Enter' to exit: ")
-            if action.lower() == "r":
-                return False  
-            elif action.lower() == "":
-                exit()
-
     except requests.exceptions.RequestException as e:
         print("An error occurred while making the request:", e)
-
-    return True  # Continue with the program
 
 def extract_urls_from_site(site_url):
     try:
@@ -65,60 +56,30 @@ def extract_urls_from_site(site_url):
 
 site_urls = ["https://nascecresceignora.it/", "https://www.everyeye.it/"]
 site_names = [urlparse(url).hostname.replace("www.", "").split(".")[0] for url in site_urls]
+
 site_names.append("Enter custom URL...")  # Add option to enter a custom URL
 
-while True:
-    # Create the website selection menu
-    website_menu = SelectionMenu(site_names, "Select the website to scrape:")
-    website_menu.show()
-    selected_site_option = website_menu.selected_option  
-    
-    if selected_site_option == len(site_names) - 1:  # Last option is "Enter custom URL..."
-        site_url = input("Enter the website URL: ")
-        if not site_url:
-            print("No website URL entered. Exiting...")
-            exit()
-        parsed_url = urlparse(site_url)
-        site_name = parsed_url.netloc.replace("www.", "").split(".")[0]
-        print(f"You have entered the website: {site_name}")
-    else:
-        if selected_site_option < len(site_urls):
-            site_url = site_urls[selected_site_option]
-            site_name = site_names[selected_site_option]
-            print(f"You have selected the website: {site_name}")
-        else:
-            print("Exiting...")
-            exit()
-    urls = extract_urls_from_site(site_url)
+selected_site = easygui.choicebox("Select the website to scrape:", "Website Selection", choices=site_names)
 
-    titles = {re.sub(r'\W+', ' ', url): url for url in urls}
+if selected_site == "Enter custom URL...":
+    site_url = easygui.enterbox("Try your luck!:")
+    if not site_url:
+        easygui.msgbox("No website URL entered. Exiting...", "Error")
+        exit()
+    parsed_url = urlparse(site_url)
+    site_name = parsed_url.netloc.replace("www.", "").split(".")[0]
+    easygui.msgbox(f"You have entered the website: {site_name}", "Website Selection")
+else:
+    site_url = site_urls[site_names.index(selected_site)]
+    site_name = selected_site
+    easygui.msgbox(f"You have selected the website: {site_name}", "Website Selection")
 
-    return_to_selection = False
+urls = extract_urls_from_site(site_url)
 
-    while True:
-        # Create the news title selection menu
-        if site_url == "https://nascecresceignora.it/":
-            menu_options = list(titles.keys())[14:-1]
-        elif site_url == "https://www.everyeye.it/":
-            menu_options = list(titles.keys())[27:-12]
-        else:
-            menu_options = list(titles.keys())
+titles = {re.sub(r'\W+', ' ', url): url for url in urls}
 
-        title_menu = SelectionMenu(menu_options, "Select a news title:")
-        title_menu.show()
-        selected_title_option = title_menu.selected_option
+selected_title = easygui.choicebox("Select a news title:", "News Titles", choices=list(titles.keys()))
 
-        print("Selected title option:", selected_title_option)
-
-        if selected_title_option == len(menu_options):
-            print("Returning to the website selection...")
-            break
-
-        if selected_title_option >= 0 and selected_title_option < len(menu_options):
-            selected_title = menu_options[selected_title_option]
-            if scrape_and_display(titles[selected_title]):
-                return_to_selection = True
-        else:
-            print("Invalid selection. Returning to the article selection.")
-
-# The code will continue running if `return_to_selection` is False
+if selected_title:
+    selected_link = titles[selected_title]
+    scrape_and_display(selected_link)
